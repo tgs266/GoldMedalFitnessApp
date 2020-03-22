fabaa5bbd6b531296623cb1858ad0afb11063a9a
tuckersiegel <siegel.tucker@gmail.com>
2020-03-22 17:13:53 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(GoldMedalFitnessApp());
+
+class GoldMedalFitnessApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
