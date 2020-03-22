722559cfdec3f0e8b33709c3599afe80c3765773
tuckersiegel <siegel.tucker@gmail.com>
2020-03-22 19:24:41 -0400

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
+import 'package:localstorage/localstorage.dart';
 
 void main() => runApp(GoldMedalFitnessApp());
+final LocalStorage main_storage = new LocalStorage('info');
+bool firstTime = main_storage.getItem("first") ?? true;
+
+double screenWidth, screenHeight;
 
 class GoldMedalFitnessApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    
+    if (firstTime == true) {
+      main_storage.setItem("hasImage", false);
+    }
+    
     return CupertinoApp(
       home: HomeScreen(),
     );
@@ -13,8 +24,57 @@ class GoldMedalFitnessApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings_solid),
+            title: Text("Settings"),
+          )
+        ]
+     ),
+     tabBuilder: (context, i) {
+       if (i == 0) {
+         return Center(child: HomeTab());
+       } else {
+         return Center(child: Text("$i"));
+       }
+     },
     );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Home")
+      ),
+      child: Column(
+        children: <Widget>[
+          BarcodeWidget()
+        ],
+      )
+    );
+  }
+}
+
+class BarcodeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (main_storage.getItem("hasImage") == false) {
+      return Image.asset(
+          "assets/no_bar_code.png",
+          height: 48.0,
+          width: 48.0
+      );
+    }
   }
 }
